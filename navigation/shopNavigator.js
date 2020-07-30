48a739bb10c1ded7e85ed633a7b784309a1d5afc
Yuvaraj R <yuvaa.it@gmail.com>
2020-07-30 18:19:53 +0200

    Display cart icon on header and navigate to separate screen

--- navigation/shopNavigator.js
+++ navigation/shopNavigator.js
@@ -5,6 +5,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import ProductsOverView from '../screen/ProductsOverviewScreen';
 import { COLORS } from '../constants/colors';
 import ProductDetails from '../screen/ProductDetailsScreen';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import CustomHeaderButton from '../components/HeaderButton';
+import CartScreen from '../screen/CartScreen';
 
 const Stack = createStackNavigator();
 const ShopNavigator = () => {
@@ -23,8 +26,15 @@ const ShopNavigator = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={NavigationOptions}>
-                <Stack.Screen name="Products" component={ProductsOverView} />
+                <Stack.Screen name="Products" component={ProductsOverView} options={({ navigation }) => ({
+                    headerRight: () => (<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                        <Item title="cart" iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'} onPress={() => {
+                            navigation.navigate("Cart")
+                        }}></Item>
+                    </HeaderButtons>)
+                })} />
                 <Stack.Screen name="ProductDetails" component={ProductDetails} options={({ route }) => ({ title: route.params.title })} />
+                <Stack.Screen name="Cart" component={CartScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
---
+++ screen/CartScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from "react-native";
+
+const CartScreen = () => {
+    return (
+        <View>
+            <Text>Cart</Text>
+        </View>
+    )
+}
+export default CartScreen;
